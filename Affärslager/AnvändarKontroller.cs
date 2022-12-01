fcb5c8ea0d387eb02b335f53ea4be6364508ba23
Henric Johansson <s2107575@student.hb.se>
2022-12-01 14:41:42 +0100

    asd

--- Affärslager/AnvändarKontroller.cs
+++ Affärslager/AnvändarKontroller.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Datalager;
+using Entiteter;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Affärslager
 {
-    internal class AnvändarKontroller
+    public class AnvändarKontroller
     {
+        public AnvändareRepository användareRepository = null;
+        public AnvändarKontroller()
+        {
+            användareRepository = new AnvändareRepository();
+        }
+        
+        
+        public Användare SkapaAnvändare(string användarnamn, string förnamn, string efternamn)
+        {
+            return new Användare(användarnamn, förnamn, efternamn);
+        }
+        public List<Användare> HämtaAnvändare()
+        {
+            return användareRepository.HämtaAnvändare();
+        }
     }
 }
