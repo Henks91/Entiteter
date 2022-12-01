affe21bcba2b4fa528ccc2efd42e06a34698414b
Carl Wånsander <s2108345@student.hb.se>
2022-12-01 12:12:21 +0100

    skapat repositorys

--- Datalager/Databas.cs
+++ Datalager/Databas.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using Entiteter;
+
 
 namespace Datalager
 {
-    public class Databas
+    public sealed class Databas
     {
+        private static Databas instans = null;
+        private static object lockToken = new object();
+
+        private static List<Bag> bag;
+        private static List<Klubba> klubba;
+        private static List<Användare> användare;
+
+
+        private Databas () { }
+        public static Databas Instans
+        {
+            get
+            {
+                lock (lockToken)
+                {
+                    instans ??= new Databas();
+                }
+                return instans;
+            }
+        }
     }
 }
