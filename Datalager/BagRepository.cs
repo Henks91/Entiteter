9cf6c2192c758a6032500d10dd8dfc273714a90b
Carl Wånsander <s2108345@student.hb.se>
2022-12-01 13:13:36 +0100

    Bagrepositury skapad

--- Datalager/BagRepository.cs
+++ Datalager/BagRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entiteter;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,20 @@ namespace Datalager
 {
     public class BagRepository
     {
+        private Databas databas;
 
+        public BagRepository()
+        {
+            databas = Databas.Instans;
+        }
+        public List<Bag> HämtaBag()
+        {
+            return databas.HämtaBag();
+        }
+
+        public Bag SkapaBag(Användare användare, string bagNamn)
+        {
+            return databas.SkapaBag(användare, bagNamn);
+        }
     }
 }
